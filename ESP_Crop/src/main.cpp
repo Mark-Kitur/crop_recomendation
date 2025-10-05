84c1da0c8f2341484198459a01e29c0aabda06c3
Mark-Kitur <kimutai.workspace@gmail.com>
2025-10-05 13:18:27 +0300

    add button

--- ESP_Crop/src/main.cpp
+++ ESP_Crop/src/main.cpp
@@ -7,10 +7,10 @@
 #include <interrupts.h>
 
 // WiFi credentials
-const char* ssid = "B1";
-const char* password = "RoomB1@cax123";
+const char* ssid = "GRACE D";
+const char* password = "grace019";
 
-const char* serverName = "http://192.168.2.171:5000/api/data";
+const char* serverName = "http://192.168.2.171:3000/data_points";
 
 int pin=5;
 // LCD setup
@@ -38,15 +38,9 @@ float randomval(float minVal, float maxVal) {
   return random(minVal * 10, maxVal * 10) / 10.0;
 }
 
-void isr_(){
-  lcd.begin(16, 2);
-  lcd.print("ISR");
-  delay(100);
-}
+void sendWeb(float temperature, float humidity, float Rainfall, float pH, float N, float P, float K);
 
 void setup() {
-  // pinMode(pin,INPUT);
-  // attachInterrupt(pin,isr_,RISING);
   Serial.begin(9600);
   dht.begin();
   lcd.begin(16, 2);
@@ -110,14 +104,55 @@ void loop() {
   lcd.print("C H:");
   lcd.print(humidity, 0);
 
-  //  Send data to Flask
+  // Send data to web
+  int button = analogRead(A0);
+  Serial.println(button);
+  if (button >500){
+    Serial.println(button);
+    sendWeb(temperature,humidity,Rainfall,pH,N,P,K);
+  }
+  // else{
+  //   Serial.println("Sender mode not ACTIVATED");
+  //   lcd.clear();
+  //   lcd.setCursor(0,0);
+  //   lcd.print("Sender mode");
+  //   lcd.setCursor(0,1);
+  //   lcd.print("Not Activated");
+  //   delay(500);
+  //   lcd.clear();
+  // }
+
+   delay(3000);
+}
+
+
+void sendWeb(float temperature, float humidity, float Rainfall, float pH, float N, float P, float K)
+{
+  Serial.println("Sender mode activated");
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Sender mode");
+    lcd.setCursor(0,1);
+    lcd.print("Activated");    
+    //  Send data to Flask
   if (WiFi.status() == WL_CONNECTED) {
     WiFiClient client;
     HTTPClient http;
     http.begin(client, serverName);   
     http.addHeader("Content-Type", "application/json");
+    //http.addHeader("X-DEVICE-UID", "esp8266-1234abcd");
 
-    // Create JSON payload
+    // String jsonData = "{";
+    // jsonData += "\"data_point\":{";
+    // jsonData += "\"temperature\":" + String(temperature, 1) + ",";
+    // jsonData += "\"humidity\":" + String(humidity, 1) + ",";
+    // jsonData += "\"rainfall\":" + String(Rainfall, 1) + ",";
+    // jsonData += "\"ph_value\":" + String(pH, 1) + ",";
+    // jsonData += "\"nitrogen\":" + String(N, 1) + ",";
+    // jsonData += "\"phosphorus\":" + String(P, 1) + ",";
+    // jsonData += "\"potassium\":" + String(K, 1);
+    // jsonData += "}}"; 
+
     String jsonData = "{";
     jsonData += "\"temperature\":" + String(temperature,1) + ",";
     jsonData += "\"humidity\":" + String(humidity,1) + ",";
@@ -125,24 +160,41 @@ void loop() {
     jsonData += "\"P\":" + String(P) + ",";
     jsonData += "\"K\":" + String(K) + ",";
     jsonData += "\"pH\":" + String(pH) + ",";
-    jsonData += "\"rainfall\":" + String(Rainfall) + ",";
-    jsonData += "\"predicted_crop\":\"" + String(crop) + "\"";
-    jsonData += "}";
+    jsonData += "\"rainfall\":" + String(Rainfall);
+    jsonData += "}";  
+
 
     int httpResponseCode = http.POST(jsonData);
 
     if (httpResponseCode > 0) {
       Serial.print("Data sent. Response: ");
       Serial.println(httpResponseCode);
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Data Sent");
+      lcd.setCursor(0,1);
+      lcd.print(httpResponseCode);
+      delay(2000);
     } else {
       Serial.print("Error sending data: ");
       Serial.println(httpResponseCode);
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Data Not Sent");
+      lcd.setCursor(0,1);
+      lcd.print(httpResponseCode);
+      delay(200);
     }
 
     http.end();
   } else {
     Serial.println("WiFi Disconnected");
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("NO Wifi ");
+    delay(500);
   }
 
-  delay(5000);  
+  delay(2000); 
 }
+
--- esp.py
+++ esp.py
@@ -2,11 +2,21 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/data', methods=['POST'])
+@app.route('/data_points', methods=['POST'])
 def receive_data():
-    data = request.json
-    print("Received:", data)
-    return jsonify({"status": "success", "received": data})
+    print("---- HEADERS ----")
+    print(request.headers)
+    print("---- RAW BODY ----")
+    print(request.data)
+    print("---- PARSED JSON ----")
+
+    try:
+        data = request.get_json(force=True)
+        print("Received JSON:", data)
+        return jsonify({"status": "success", "received": data}), 200
+    except Exception as e:
+        print("Error parsing JSON:", e)
+        return jsonify({"status": "error", "message": str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=3000)
